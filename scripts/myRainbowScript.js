21201054df4c6b6b78cfaef9d176e9f609bda676
christine-hya <81230662+christine-hya@users.noreply.github.com>
2021-04-29 17:38:09 +0200

    Initial commit

---
+++ scripts/myRainbowScript.js
@@ -0,0 +1,23 @@
+
+    function changeColour() {
+    document.getElementById("firstDiv").style.backgroundColor = "red";
+    }
+
+    function changeColour2() {
+        document.getElementById("secondDiv").style.backgroundColor = "orange";
+        }
+    function changeColour3() {
+            document.getElementById("thirdDiv").style.backgroundColor = "yellow";
+            }
+    function changeColour4() {
+                document.getElementById("fourthDiv").style.backgroundColor = "green";
+                }        
+    function changeColour5() {
+                    document.getElementById("fifthDiv").style.backgroundColor = "blue";
+                    } 
+    function changeColour6() {
+        document.getElementById("sixthDiv").style.backgroundColor = "darkviolet";
+    }  
+    function changeColour7() {
+        document.getElementById("seventhDiv").style.backgroundColor = "purple";
+    }              
